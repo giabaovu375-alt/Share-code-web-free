4be2fc751271d96dda26760087eb72f6ecbf280c
giabaovu375-alt <giabaovu375@gmail.com>
2026-04-29 17:37:56 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,16 +1,16 @@
-// ========== FIREBASE CONFIG (CHUẨN MODULE V12) ==========
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+// ========== FIREBASE CONFIG (CHUẨN MODULE) ==========
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAASY-FQ0p1rTB-WwU9I1iaWVsVk4D-O6M",
-  authDomain: "codehub-9e1b4.firebaseapp.com",
-  projectId: "codehub-9e1b4",
-  storageBucket: "codehub-9e1b4.firebasestorage.app",
-  messagingSenderId: "714965808705",
-  appId: "1:714965808705:web:c6f44f0ca869db56c838c0",
-  measurementId: "G-JZX5SX1Y3K"
+    apiKey: "AIzaSyAASY-FQ0p1rTB-WwU9I1iaWVsVk4D-O6M",
+    authDomain: "codehub-9e1b4.firebaseapp.com",
+    projectId: "codehub-9e1b4",
+    storageBucket: "codehub-9e1b4.firebasestorage.app",
+    messagingSenderId: "714965808705",
+    appId: "1:714965808705:web:c6f44f0ca869db56c838c0",
+    measurementId: "G-JZX5SX1Y3K"
 };
 
 const app = initializeApp(firebaseConfig);
